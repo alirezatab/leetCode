3205cce53e5ac7323a6cea26986f1989a1642564
alirezatab <alirezatabrizi10@gmail.com>
2025-01-07 15:13:27 -0800

    completed 1133_LargestuniqueNumber was done only in swift using HashMap and counting

---
+++ 1133_LargestUniqueNumber/largestUniqueNumber.playground/Contents.swift
@@ -0,0 +1,53 @@
+import UIKit
+
+// Time: (n + k), go though nums, go through all unique Counter which is the size of max allowed 1000
+// Space: o(k)
+// Counting
+
+func largestUniqueNumberCounter(_ nums: [Int]) -> Int {
+  var uniqeCounter = [Int](repeating: 0, count: 1001)
+  
+  for num in nums {
+    uniqeCounter[num] += 1
+  }
+  
+  for numIndex in uniqeCounter.indices.reversed() { // Stride(from: , to: by: )
+    if uniqeCounter[numIndex] == 1 {
+      return numIndex
+    }
+  }
+  
+  return -1
+}
+
+print(largestUniqueNumberCounter([5,7,3,9,4,9,8,3,1]))
+print(largestUniqueNumberCounter([9,9,8,8]))
+
+
+// HasMap
+// Time: (n)
+// Space: o(n)
+func largestUniqueNumber(_ nums: [Int]) -> Int {
+  var frequencyCounter = [Int:Int]()
+  
+  for num in nums {
+    let value = frequencyCounter[num]
+    frequencyCounter[num] = value == nil ? 1 : value! + 1
+    // if var counter = frequencyCounter[num] {
+    //     frequencyCounter[num] += 1
+    // } else {
+    //     frequencyCounter[num] = 1
+    // }
+  }
+  var largestNum = -1
+  for num in frequencyCounter.keys {
+    if frequencyCounter[num] == 1 && num > largestNum {
+      largestNum = num
+    }
+  }
+  
+  return largestNum
+}
+
+print(largestUniqueNumber([5,7,3,9,4,9,8,3,1]))
+print(largestUniqueNumber([9,9,8,8]))
