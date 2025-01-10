8b19b87f7f8e07898eb20144738afdd9e2b05704
alirezatab <alirezatabrizi10@gmail.com>
2025-01-09 21:32:39 -0800

    complete 771_class Solution {     // Time Complexity: O(J.length+S.length). The O(J.length) part comes from creating J. The O(S.length) part comes from searching S.
        // Space Complexity: O(J.length).
        func numJewelsInStones(_ jewels: String, _ stones: String) -> Int {
            var jewelsSet = Set<Character>()
            for char in jewels {
                jewelsSet.insert(char)
            }
    
            var result = 0
            for char in stones {
                if jewelsSet.contains(char) {
                    result += 1
                }
            }
    
            return result
    
       }
    }

---
+++ 771_JewelsAndStones/jewelsAndStones.playground/Contents.swift
@@ -0,0 +1,25 @@
+import UIKit
+
+
+// Time Complexity: O(J.length+S.length). The O(J.length) part comes from creating J. The O(S.length) part comes from searching S.
+
+// Space Complexity: O(J.length).
+func numJewelsInStones(_ jewels: String, _ stones: String) -> Int {
+  var jewelsSet = Set<Character>()
+  for char in jewels {
+    jewelsSet.insert(char)
+  }
+  
+  var result = 0
+  for char in stones {
+    if jewelsSet.contains(char) {
+      result += 1
+    }
+  }
+  
+  return result
+}
+
+print(numJewelsInStones("aA", "aAAbbbb"))
+print(numJewelsInStones("z", "ZZ"))
+
