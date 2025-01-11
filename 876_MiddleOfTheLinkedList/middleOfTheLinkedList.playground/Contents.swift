680a09e99c76ee7429dc1f5a55c57b875ef713ae
alirezatab <alirezatabrizi10@gmail.com>
2025-01-10 17:01:25 -0800

    completed 876_MiddleOfTheLinkedList in swift

---
+++ 876_MiddleOfTheLinkedList/middleOfTheLinkedList.playground/Contents.swift
@@ -0,0 +1,32 @@
+import UIKit
+
+//Definition for singly-linked list.
+public class ListNode {
+  public var val: Int
+  public var next: ListNode?
+  public init() { self.val = 0; self.next = nil; }
+  public init(_ val: Int) { self.val = val; self.next = nil; }
+  public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+}
+/*       s
+             f   while fast != nil && fast.next != nil
+    [1,2,3,4,5]
+
+           s
+                  f
+    [1,2,3,4,5,6]
+*/
+
+// Time: o(n)
+// Space: o(1)
+func middleNode(_ head: ListNode?) -> ListNode? {
+  var fast = head
+  var slow = head
+  
+  while fast != nil && fast?.next != nil {
+    slow = slow?.next
+    fast = fast?.next?.next
+  }
+  
+  return slow
+}
