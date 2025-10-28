aadd3a27dccc5506d0dcf8d173b435a6d67a9fc2
alirezatab <alirezatabrizi10@gmail.com>
2025-10-27 23:19:55 -0700

    replaced the whole content of 383_Ransome Note since the new way has some extra notes and different way od doing this problem

--- 383_RansomeNote/RansomeNote.playground/Contents.swift
+++ 383_RansomeNote/RansomeNote.playground/Contents.swift
@@ -1,8 +1,15 @@
-import Foundation
-
 /*
 {a: 2}
 {a: 2, b: 1}
+
+{a:2}
+
+for ransomeNoteDict, key and value    ------ a -> {a: 2, b: 1}
+    if key exists insode magazine, and the value of ransom not > magazine key {
+        return false
+    }
+
+
 */
 
 
@@ -10,64 +17,92 @@ import Foundation
 // Time: o(m)
 // Space: o(k) --> where k can not be more than 26 so o(1)
 /*
-func canConstruct(_ ransomNote: String, _ magazine: String) -> Bool {
-    guard ransomNote.count <= magazine.count else { return false }
+class Solution {
+    func canConstruct(_ ransomNote: String, _ magazine: String) -> Bool {
+        guard ransomNote.count <= magazine.count else { return false }
 
 
-    var magDict = [Character: Int]()
-    var ransomDict = [Character: Int]()
-    for char in magazine {
-        magDict[char, default: 0] += 1
-    }
-    
-    for char in ransomNote {
-        ransomDict[char, default: 0] += 1
-    }
-
-    for (char, val) in magDict {
-        if let v = ransomDict[char], v <= val {
-            ransomDict[char] = nil
+        var magDict = [Character: Int]()
+        var ransomDict = [Character: Int]()
+        for char in magazine {
+            magDict[char, default: 0] += 1
         }
-    }
+        
+        for char in ransomNote {
+            ransomDict[char, default: 0] += 1
+        }
 
-    return ransomDict.isEmpty
+        for (char, val) in magDict {
+            if let v = ransomDict[char], v <= val {
+                ransomDict[char] = nil
+            }
+        }
+
+        return ransomDict.isEmpty
+    }
+}
+*/
+
+// one dict but simpler
+class Solution {
+    func canConstruct(_ ransomNote: String, _ magazine: String) -> Bool {
+        guard ransomNote.count <= magazine.count else { return false }
+    
+        var ransomDic = [Character: Int]()
+        for char in ransomNote {
+            ransomDic[char, default: 0] += 1
+        }
+        
+        for char in magazine {
+            guard let value = ransomDic[char] else { continue }
+            ransomDic[char] = value - 1 == 0 ? nil : value - 1
+        }
+        
+        return ransomDic.isEmpty
+    }
+}
+
+
+
+// one dict
+/*
+class Solution {
+    func canConstruct(_ ransomNote: String, _ magazine: String) -> Bool {
+        guard ransomNote.count <= magazine.count else { return false }
+
+        var magDict = [Character: Int]()
+        for char in magazine {
+            magDict[char, default: 0] += 1
+        }
+
+        for char in ransomNote {
+            if let val = magDict[char], val > 0 {
+                magDict[char] = val - 1
+            } else {
+                return false
+            }
+        }
+
+        return true
+    }
 }
 */
 
 
-// one dict
-func canConstruct(_ ransomNote: String, _ magazine: String) -> Bool {
-  guard ransomNote.count <= magazine.count else { return false }
-  
-  var magDict = [Character: Int]()
-  for char in magazine {
-    magDict[char, default: 0] += 1
-  }
-  
-  for char in ransomNote {
-    if let val = magDict[char], val > 0 {
-      magDict[char] = val - 1
-    } else {
-      return false
-    }
-  }
-  
-  return true
-}
-
-
 /*
-func canConstruct(_ ransomNote: String, _ magazine: String) -> Bool {
-    var magazineCopy = magazine
+class Solution {
+    func canConstruct(_ ransomNote: String, _ magazine: String) -> Bool {
+        var magazineCopy = magazine
 
-    for letter in ransomNote {
-        if let index = magazineCopy.firstIndex(of: letter) {
-            magazineCopy.remove(at: index)
-        } else {
-            return false
+        for letter in ransomNote {
+            if let index = magazineCopy.firstIndex(of: letter) {
+                magazineCopy.remove(at: index)
+            } else {
+                return false
+            }
         }
-    }
 
-    return true
+        return true
+    }
 }
 */
