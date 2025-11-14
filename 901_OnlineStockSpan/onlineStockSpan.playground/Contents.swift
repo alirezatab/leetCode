15adfa5e59390c1975c1ace8d6caab0c9624f35a
alirezatab <alirezatabrizi10@gmail.com>
2025-11-14 13:11:41 -0800

    updated 901_OnlineStockSpan with comments for diagramming as part of LeetCode Data Structure and Algorithms course in swift only

--- 901_OnlineStockSpan/onlineStockSpan.playground/Contents.swift
+++ 901_OnlineStockSpan/onlineStockSpan.playground/Contents.swift
@@ -6,23 +6,66 @@
 // Space: o(n)
 // In the worst case scenario for space (when all the stock prices are decreasing), the while loop will never run, which means the stack grows to a size of n.
 
+/*
+    (starting from that day and going backward) --> hint at using a stack probably
+    stock price <= price of that day? which day
+    so if stick price is 8$ today, span = previousDays <= 8
+
+    so in inti, initilize the stack
+    stack -- append and popLast-> s = []
+
+    100 -> [100], return 1.. if stack is empty, add it to stack and return 1
+    80 -> [100, 80], return 1.. if top of the stack is greater than cur price...
+                        100 > 80, return 1
+    60 -> [100, 80, 60], 80 > 60 return 1..
+    70 -> [100, 80, 60], 70 <= 60 pop 60, and add 1 + 1 = 2, so return 2
+            1 is the previous number of days... how to track that?
+            * push a tuple, (price, span)
+            * index or index + 1 --> cant go with index only, since we nned the price also
+    [100:1, 80:1, 60:1] --> 70 --> pop tha stack, and add 70
+
+    [100,80, 70] --> 70 --> pop tha stack, and add 70,
+           return { lastSpan + currentSpan } == 1 + 1 = 2
+
+    [100: 1,80:1, 70:2, 60:1] 60 <= 75 so we pop 1 + 1 = 2
+    [100: 1,80:1, 70:2] 70 <= 75 so we pop 2 + 2 = 4
+    [100: 1,80:1] 80 > 75, so we append 75
+     
+
+    [100: 1,80:1, 75:4] 80 > 75, return spane
+
+    so cause of how we do it this is a monitobic stack, non-increasing
+
+    func next
+    var span = 1
+    while stack is not empty && price at top of the stack <= price
+        span += lastStocksSpan
+        pop top of the stack
+    
+    add it to the top of the stack, the number of days
+    return result
+*/
+
+// Time: o(n)
+// Space: o(n)
 class StockSpanner {
 
-    var stack: [(price: Int, count: Int)]
+    var stocks: [(price: Int, span: Int)] // Stack
+
 
     init() {
-        stack = [ ]
+        stocks = [ ]
     }
     
     func next(_ price: Int) -> Int {
-        var count = 1
-        while let lastStock = stack.last, lastStock.price <= price {
-            count += lastStock.count
-            stack.popLast()
+        var span = 1
+        while let lastStock = stocks.last, lastStock.price <= price {
+            span += lastStock.span
+            stocks.popLast()
         }
-        stack.append((price: price, count: count))
 
-        return count
+        stocks.append((price: price, span: span))
+        return span
     }
 }
 
