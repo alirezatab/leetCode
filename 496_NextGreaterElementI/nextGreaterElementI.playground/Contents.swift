7f90c4c5ee67d1fd3a39a866b7f138714196805c
alirezatab <alirezatabrizi10@gmail.com>
2025-11-14 16:27:24 -0800

    updated 496_NextGreaterElementI with comments for diagramming as part of LeetCode Data Structure and Algorithms course in swift only

--- 496_NextGreaterElementI/nextGreaterElementI.playground/Contents.swift
+++ 496_NextGreaterElementI/nextGreaterElementI.playground/Contents.swift
@@ -1,4 +1,45 @@
+/*
+    nums1 = [4,1,2], nums2 = [1,3,4,2]
 
+    map the number to the next grater element
+    {1:3, 3: 4, 4: -1, 2: -1} --> monotonic Stack cause you care about current price - the last parice
+
+    monotonic Incereasing didnt work
+    monotonic non-increasing didnt work
+    [1]
+    [1] -> check the top, if 1 < 3 --> then add to map {top of stack(key): cur(value)}
+    pop
+    stack = {1:3} => stack = []
+    append stack = [3]
+
+    [3]-> check the top, if 3 < 4 --> pop, then add to map {top of stack(key): cur(value)}
+    stack = {1:3, 3:4}
+    then append stack = [4]
+    
+    [4] -> check the top, if 4 is not < 2 --> so we append to stack
+    [4, 2]
+
+    var res = []
+             i
+    nums1 = [4,1,2] {1:3, 3:4} --> no 4, append -1
+    
+    var res = [-1]
+               i
+    nums1 = [4,1,2] {1:3, 3:4} --> yes 1, append 3
+
+    var res = [-1, 3, -1]
+                 i
+    nums1 = [4,1,2] {1:3, 3:4} --> no 2, append -1
+    reurtn res
+
+    var result
+    for loop through nums1
+    if key existsi, append value to result
+    else append -1
+*/
+
+// Time: o(n) --> nums2 = n, nums1 = m, sinc m is a subset of n, we can say m < n so we just say n
+// space: o(n)
 func nextGreaterElement(_ nums1: [Int], _ nums2: [Int]) -> [Int] {
   var stack = [Int]()
   var dict = [Int: Int]()
